eb758863d52fc14c19fe9aa6148a67894faceb1c
porterest <191361776+porterest@users.noreply.github.com>
2024-12-17 20:00:36 +0300

    mow

--- backend/temp/api.py
+++ backend/temp/api.py
@@ -20,7 +20,7 @@ app = FastAPI()
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
     logging.error(f"{request}: {exc_str}")
-    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    content = {'status_code': 422, 'message': exc_str, 'data': None}
     return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
