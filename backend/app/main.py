21e38bf5fed477cb31d85e6ac6209d46a1860849
granolic <191360009+granolic@users.noreply.github.com>
2024-12-26 17:57:29 +0300

    meow (backend)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+import logging
+
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
 
 from middlewares import check_for_auth
 from routes import (
@@ -15,6 +19,19 @@ from routes import (
 from settings import settings
 
 app = FastAPI()
+logger = logging.getLogger(__name__)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError,
+):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    logger.error(f"{request}: {exc_str}")
+    content = {'status_code': 422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 app.add_middleware(
     CORSMiddleware,
