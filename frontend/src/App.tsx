05322fc953c433d27430fbd7c4f34afde5aeaa92
porterest <191361776+porterest@users.noreply.github.com>
2024-12-29 05:31:07 +0300

    meow (frontend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,26 +14,29 @@ import Layout from "./components/Layout";
 import { CandleDataProvider } from "./context/CandleDataContext";
 import { AuthProvider } from "./context/AuthContext";
 import WalletHandler from "./components/WalletHandler";
+import { SceneProvider } from "./components/SceneProvider";
 
 const App: React.FC = () => {
   return (
     <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/porterest/wp-front/refs/heads/main/frontend/public/tonconnect-manifest.json">
       <AuthProvider>
         <UserBalanceProvider>
-          <CandleDataProvider>
-            <Router>
-              <Layout>
-                <Routes>
-                  <Route path="/" element={<Navigate to="/home" />} />
-                  <Route path="/home" element={<HomePage />} />
-                  <Route path="/balance" element={<BalancePage />} />
-                  <Route path="/game" element={<GamePage />} />
-                  <Route path="/profile" element={<ProfilePage />} />
-                </Routes>
-              </Layout>
-              <WalletHandler />
-            </Router>
-          </CandleDataProvider>
+          <SceneProvider>
+            <CandleDataProvider>
+              <Router>
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/balance" element={<BalancePage />} />
+                    <Route path="/game" element={<GamePage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                  </Routes>
+                </Layout>
+                <WalletHandler />
+              </Router>
+            </CandleDataProvider>
+          </SceneProvider>
         </UserBalanceProvider>
       </AuthProvider>
     </TonConnectUIProvider>
