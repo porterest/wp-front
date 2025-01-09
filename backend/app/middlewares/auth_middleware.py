e4b290a2901ad485f860f37443a10214d3c29596
porterest <191361776+porterest@users.noreply.github.com>
2025-01-10 01:58:34 +0300

    401 with empty token

--- backend/app/middlewares/auth_middleware.py
+++ backend/app/middlewares/auth_middleware.py
@@ -18,7 +18,16 @@ async def check_for_auth(
         response = await call_next(request)
         return response
 
-    access_token = request.headers.get('Authorization').replace('Bearer ', '')
+    if 'Authorization' not in request.headers:
+        return JSONResponse(
+            status_code=401,
+            content={
+                'detail': 'Token is empty',
+            }
+        )
+
+    access_token = request.headers['Authorization'].replace('Bearer ', '')
+
 
     auth_service = get_auth_service()
     try:
