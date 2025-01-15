ec67994b5f1255dfbd17f80fb62f099aa2480fcc
porterest <191361776+porterest@users.noreply.github.com>
2025-01-15 18:24:30 +0300

    meow (backend)

--- backend/app/dependencies/services/block.py
+++ backend/app/dependencies/services/block.py
@@ -2,6 +2,7 @@ from abstractions.services.block import BlockServiceInterface
 from dependencies.math.aggregate_bets import get_aggregate_bets_service
 from dependencies.repositories.block import get_block_repository
 from dependencies.repositories.chain import get_chain_repository
+from dependencies.repositories.user import get_user_repository
 from dependencies.services.bet import get_bet_service
 from services.BlockService import BlockService
 
@@ -11,5 +12,6 @@ def get_block_service() -> BlockServiceInterface:
         block_repository=get_block_repository(),
         bet_service=get_bet_service(),
         aggregate_bets_service=get_aggregate_bets_service(),
-        chain_repository=get_chain_repository()
+        chain_repository=get_chain_repository(),
+        user_repository=get_user_repository()
     )
