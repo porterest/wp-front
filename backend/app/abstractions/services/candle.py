ff68970c524dde3d4a3c90e4b48dce8c9b97bad5
porterest <191361776+porterest@users.noreply.github.com>
2025-01-15 13:44:57 +0300

    meow (backend)

--- backend/app/abstractions/services/candle.py
+++ backend/app/abstractions/services/candle.py
@@ -6,9 +6,9 @@ from domain.models.block import Block
 
 class CandleServiceInterface(ABC):
 
-    @abstractmethod
-    async def get_candles(self, pair_id: str) -> list[Candle]:
-        ...
+    # @abstractmethod
+    # async def get_candles(self, pair_id: str) -> list[Candle]:
+    #     ...
     @abstractmethod
     async def get_n_last_blocks_by_pair_id(self, pair_id: str, n: int) -> list[Block]:
         ...
--- backend/app/services/candle.py
+++ backend/app/services/candle.py
@@ -1,12 +1,16 @@
 from typing import List
+from uuid import UUID
 
 from abstractions.services.block import BlockServiceInterface
 from abstractions.services.candle import CandleServiceInterface
 from domain.metaholder.responses.candle import Candle
-from services.BlockService import BlockService
 
 
 class CandleService(CandleServiceInterface):
     block_service: BlockServiceInterface
-    async def get_candles(self, pair_id: str) -> List[Candle]:
-        blocks = self.block_service.get_n_last_blocks_by_pair_id(pair_id=pair_id, n = 10)
+
+    # async def get_candles(self, pair_id: str) -> List[Candle]:
+    #     blocks = self.block_service.get_n_last_blocks_by_pair_id(pair_id=pair_id, n=10)
+
+    async def get_n_last_blocks_by_pair_id(self, pair_id: UUID, n: int):
+        return await self.block_service.get_n_last_blocks_by_pair_id(pair_id=pair_id, n=n)
