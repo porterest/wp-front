c920b90f205840fbdc5b672a9fe20fb4c9bfd6ff
porterest <191361776+porterest@users.noreply.github.com>
2025-01-18 18:47:45 +0300

    meow (frontend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,31 +12,30 @@ import GamePage from "./pages/GamePage";
 import ProfilePage from "./pages/ProfilePage";
 import Layout from "./components/Layout";
 import { CandleDataProvider } from "./context/CandleDataContext";
-// import { AuthProvider } from "./context/AuthContext";
+import { DataPrefetchProvider } from "./context/DataPrefetchContext"; // Добавлено
 import WalletHandler from "./components/WalletHandler";
-// import { SceneProvider } from "./components/SceneProvider";
 
 const App: React.FC = () => {
   return (
     <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/porterest/wp-front/refs/heads/main/frontend/public/tonconnect-manifest.json">
-      {/*<AuthProvider>*/}
-        <UserBalanceProvider>
-            <CandleDataProvider>
-              <Router>
-                <Layout>
-                  <Routes>
-                    <Route path="/" element={<Navigate to="/home" />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/balance" element={<BalancePage />} />
-                    <Route path="/game" element={<GamePage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                  </Routes>
-                </Layout>
-                <WalletHandler />
-              </Router>
-            </CandleDataProvider>
-        </UserBalanceProvider>
-      {/*</AuthProvider>*/}
+      <UserBalanceProvider>
+        <CandleDataProvider>
+          <DataPrefetchProvider> {/* Оборачиваем в DataPrefetchProvider */}
+            <Router>
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<Navigate to="/home" />} />
+                  <Route path="/home" element={<HomePage />} />
+                  <Route path="/balance" element={<BalancePage />} />
+                  <Route path="/game" element={<GamePage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                </Routes>
+              </Layout>
+              <WalletHandler />
+            </Router>
+          </DataPrefetchProvider>
+        </CandleDataProvider>
+      </UserBalanceProvider>
     </TonConnectUIProvider>
   );
 };
