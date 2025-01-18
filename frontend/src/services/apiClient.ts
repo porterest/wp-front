96074de4655c7a6bfb899a24af90db9cb2c1b3cd
porterest <191361776+porterest@users.noreply.github.com>
2025-01-18 15:37:41 +0300

    meow (frontend)

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -78,14 +78,15 @@ apiClient.interceptors.response.use(
     };
 
     console.log('before if')
+    console.log("error.response?.status", "originalRequest._retry","refreshTokenFunction")
+    console.log(error.response?.status, originalRequest._retry, refreshTokenFunction)
     // Если токен истек (401) и запрос не был повторен ранее
     if (
       error.response?.status === 401 &&
       !originalRequest._retry &&
       refreshTokenFunction
     ) {
-      console.log("error.response?.status", "originalRequest._retry","refreshTokenFunction")
-      console.log(error.response?.status, originalRequest._retry, refreshTokenFunction)
+
       originalRequest._retry = true;
 
       try {
