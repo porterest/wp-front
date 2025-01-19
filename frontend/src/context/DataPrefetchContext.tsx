6a63f453df462f7f454d08f81ddf56dce8484056
porterest <191361776+porterest@users.noreply.github.com>
2025-01-19 14:31:55 +0300

    meow (frontend)

--- frontend/src/context/DataPrefetchContext.tsx
+++ frontend/src/context/DataPrefetchContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { fetchCandles } from "../services/api";
 import { PairOption } from "../types/pair";
 import { CandleData } from "../types/candles";
