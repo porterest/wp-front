6e1899af9b9dfc6e99850f13f634bff0ce120051
porterest <191361776+porterest@users.noreply.github.com>
2025-01-19 17:21:24 +0300

    meow (frontend)

--- frontend/src/components/BetArrow.tsx
+++ frontend/src/components/BetArrow.tsx
@@ -85,7 +85,7 @@ const BetArrow: React.FC<BetArrowProps> = ({
 
       {/* Текст депозита */}
       <Text
-        position={[1, 5.1, 1]}
+        position={[0, 5.1, 1]}
         fontSize={0.3}
         color="lightgreen"
         anchorX="center"
