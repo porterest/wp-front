26e289a0b0bf47f49bf575dc86c2833b1a3ed0b6
porterest <191361776+porterest@users.noreply.github.com>
2025-01-26 20:56:21 +0300

    meow (backend)

--- backend/app/domain/models/bet.py
+++ backend/app/domain/models/bet.py
@@ -4,7 +4,6 @@ from typing import TypeVar, Annotated, Optional
 from uuid import UUID
 
 from domain.enums import BetStatus
-from domain.models import Pair
 from domain.models.base import BaseModel
 
 BetVector = TypeVar('BetVector', bound=tuple[
@@ -14,12 +13,13 @@ BetVector = TypeVar('BetVector', bound=tuple[
 
 if typing.TYPE_CHECKING:
     from domain.models.user import User
+    from domain.models.pair import Pair
 
 
 @dataclass(kw_only=True)
 class Bet(BaseModel):
     user: Optional['User'] = None
-    pair: Optional[Pair] = None
+    pair: Optional['Pair'] = None
     amount: float
     block_number: int
     vector: BetVector
