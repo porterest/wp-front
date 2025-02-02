f71183e9c3dee7eeed6408de29d837ea5bbc7f4d
porterest <191361776+porterest@users.noreply.github.com>
2025-02-02 15:50:31 +0300

    meow (backend)

--- backend/app/dependencies/services/inner_token.py
+++ backend/app/dependencies/services/inner_token.py
@@ -3,11 +3,13 @@ from dependencies.repositories.user import get_user_repository
 from dependencies.services.app_wallet.service import get_app_wallet_service
 from dependencies.services.ton.client import get_ton_client
 from services.InnerToken import InnerTokenService
+from settings import settings
 
 
 def get_inner_token_service() -> InnerTokenInterface:
     return InnerTokenService(
         ton_client=get_ton_client(),
         user_repository=get_user_repository(),
-        app_wallet_provider=get_app_wallet_service()
+        app_wallet_provider=get_app_wallet_service(),
+        token_minter_address_str=settings.inner_token.minter_address,
     )
