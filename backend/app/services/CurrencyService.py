7f2d5ffccf95a0354fa3e36b4b89990c4701871b
porterest <191361776+porterest@users.noreply.github.com>
2025-02-03 15:40:08 +0300

    meow (backend)

--- backend/app/services/CurrencyService.py
+++ backend/app/services/CurrencyService.py
@@ -1,7 +1,10 @@
+import logging
 from dataclasses import dataclass
 
 from abstractions.services.currency import CurrencyServiceInterface
 from abstractions.services.inner_token import InnerTokenInterface
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -11,4 +14,7 @@ class CurrencyService(CurrencyServiceInterface):
     pool_address: str = 'EQCsJuz0ilim-brei8rjwx9KMg3pUVD9Iviyxvzt7ge3Ik46'
 
     async def convert_ton_to_inner_token(self, amount: float) -> float:
-        return amount / await self.inner_token_service.get_token_price(self.pool_address)
+        logger.info(amount)
+        price = await self.inner_token_service.get_token_price(self.pool_address)
+        logger.info(price)
+        return amount / price
--- backend/app/services/ton/client/main_client.py
+++ backend/app/services/ton/client/main_client.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass
 
 from pytoniq_core import Address
@@ -5,6 +6,8 @@ from pytoniq_core import Address
 from abstractions.services.tonclient import TonClientInterface, Nano
 from domain.models.app_wallet import AppWalletWithPrivateData
 from services.ton.client.base import AbstractBaseTonClient
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -20,22 +23,23 @@ class MainTonClient(AbstractBaseTonClient):
             pool_address: str,
     ) -> None:
         await self.ton_client.provide_liquidity(ton_amount=ton_amount, jetton_amount=jetton_amount,
-                                                    admin_wallet=admin_wallet, pool_address=pool_address)
+                                                admin_wallet=admin_wallet, pool_address=pool_address)
 
     async def remove_liquidity(self, ton_amount: float, jetton_amount: float, admin_wallet: AppWalletWithPrivateData,
                                pool_address: str) -> None:
         await self.ton_client.remove_liquidity(ton_amount=ton_amount, jetton_amount=jetton_amount,
-                                                   admin_wallet=admin_wallet, pool_address=pool_address)
+                                               admin_wallet=admin_wallet, pool_address=pool_address)
 
     async def get_pool_reserves(self, pool_address: Address) -> tuple[float, float]:
         try:
             return await self.ton_client.get_pool_reserves(pool_address=pool_address)
         except:
+            logger.error("excepted", exc_info=True)
             return await self.ton_api_client.get_pool_reserves(pool_address=pool_address.to_str())
 
     async def get_jetton_wallet_address(self, contract_address: Address, target_address: Address) -> Address:
         return await self.ton_client.get_jetton_wallet_address(contract_address=contract_address,
-                                                                   target_address=target_address)
+                                                               target_address=target_address)
 
     async def get_public_key(self, address: str):
         return await self.ton_api_client.get_public_key(address)
