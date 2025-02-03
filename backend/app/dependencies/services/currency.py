1d8192792fe01211bc00d282a5706b64a499d1b2
porterest <191361776+porterest@users.noreply.github.com>
2025-02-03 15:21:54 +0300

    meow (backend)

--- backend/app/dependencies/services/currency.py
+++ backend/app/dependencies/services/currency.py
@@ -1,6 +1,9 @@
 from abstractions.services.currency import CurrencyServiceInterface
+from dependencies.services.inner_token import get_inner_token_service
 from services.CurrencyService import CurrencyService
 
 
 def get_currency_service() -> CurrencyServiceInterface:
-    return CurrencyService()
+    return CurrencyService(
+        inner_token_service=get_inner_token_service(),
+    )
