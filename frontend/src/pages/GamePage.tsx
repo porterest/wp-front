f2a590ad26731e3717ea04e40d4f182bad64cedc
porterest <191361776+porterest@users.noreply.github.com>
2025-02-08 18:58:40 +0300

    meow (frontend)

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -270,7 +270,7 @@ const GamePage: React.FC = () => {
         />
       </div>
       <Scene
-        // orbitControlsEnabled={orbitControlsEnabled}
+        orbitControlsEnabled={orbitControlsEnabled}
         data={data.candles || []}
         onScaleReady={(scales) => {
           console.log("Scales from Scene:", scales);
