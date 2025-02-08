5e12d1b0b7ba081cda751e100b268105e793827a
porterest <191361776+porterest@users.noreply.github.com>
2025-02-08 18:25:26 +0300

    meow (frontend)

--- frontend/src/components/Scene.tsx
+++ frontend/src/components/Scene.tsx
@@ -1,109 +1,85 @@
-import React, { useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import { ScaleProvider, useScale } from "../context/ScaleContext";
-import { OrbitControls, GizmoHelper, GizmoViewport } from "@react-three/drei";
 import { ScaleFunctions } from "../types/scale";
 import { CandleData } from "../types/candles";
+import * as THREE from "three";
+
 
 interface SceneProps {
   children: React.ReactNode;
-  orbitControlsEnabled: boolean;
   data: CandleData[];
   onScaleReady: (scaleFunctions: ScaleFunctions) => void;
   style?: React.CSSProperties;
 }
 
-const Scene: React.FC<SceneProps> = ({ children, data, onScaleReady }) => {
-  return (
-    <Canvas camera={{ position: [10, 10, 10], fov: 60 }}>
-      {/*<OrbitControls enableZoom={true} enableRotate={orbitControlsEnabled} enablePan={orbitControlsEnabled} />*/}
-      <OrbitControls enableRotate={false} enablePan={false} enableZoom={false} />
+const RotatingGraph: React.FC<{ isRotating: boolean; children: React.ReactNode }> = ({ isRotating, children }) => {
+  const groupRef = useRef<THREE.Group>(null);
 
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 10]} intensity={1} castShadow={true} />
+  useFrame(() => {
+    if (isRotating && groupRef.current) {
+      // Вращаем группу вокруг оси Y
+      groupRef.current.rotation.y += 0.01;
+    }
+  });
 
-      <ScaleProvider data={data}>
-        {children}
-        <ScaleHandler onScaleReady={onScaleReady} />
-      </ScaleProvider>
-
-      <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-        <GizmoViewport
-          axisColors={["red", "green", "blue"]}
-          labelColor="black"
-          /* Дополнительно можно настроить стили и поведение */
-        />
-      </GizmoHelper>
-    </Canvas>
-  );
+  return <group ref={groupRef}>{children}</group>;
 };
 
+const Scene: React.FC<SceneProps> = ({ children, data, onScaleReady }) => {
+  const [isRotating, setIsRotating] = useState(false);
+
+  return (
+    <>
+      <Canvas camera={{ position: [10, 10, 10], fov: 60 }}>
+        {/* Можно отключить встроенное управление вращением */}
+        <OrbitControls enableRotate={false} enablePan={false} enableZoom={false} />
+
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 10]} intensity={1} castShadow />
+
+        <ScaleProvider data={data}>
+          <RotatingGraph isRotating={isRotating}>
+            {children}
+          </RotatingGraph>
+          <ScaleHandler onScaleReady={onScaleReady} />
+        </ScaleProvider>
+      </Canvas>
+
+      {/* Кружочек в углу для управления вращением */}
+      <div
+        onClick={() => setIsRotating((prev) => !prev)}
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          width: "50px",
+          height: "50px",
+          borderRadius: "50%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          cursor: "pointer",
+          userSelect: "none",
+        }}
+      >
+        {isRotating ? "Стоп" : "Вращать"}
+      </div>
+    </>
+  );
+};
 
 const ScaleHandler: React.FC<{ onScaleReady: (scaleFunctions: ScaleFunctions) => void }> = ({ onScaleReady }) => {
   const scaleFunctions = useScale();
 
   useEffect(() => {
     onScaleReady(scaleFunctions);
-    // console.log('scales exposed', scaleFunctions);
   }, [onScaleReady, scaleFunctions]);
 
   return null;
 };
 
 export default Scene;
-
-
-
-
-// import React, { useEffect } from "react";
-// import { Canvas } from "@react-three/fiber";
-// import { OrbitControls } from "@react-three/drei";
-// import { ScaleProvider, useScale } from "../context/ScaleContext";
-// import { ScaleFunctions } from "../types/scale";
-// import { CandleData } from "../types/candles";
-// // import { CandleDataContextValue } from "../types/candles";
-//
-//
-// interface SceneProps {
-//   children: React.ReactNode;
-//   orbitControlsEnabled: boolean;
-//   data: CandleData[];
-//   onScaleReady: (scaleFunctions: ScaleFunctions) => void; // Callback для передачи функций
-// }
-//
-// const Scene: React.FC<SceneProps> = ({ children, orbitControlsEnabled, data, onScaleReady }) => {
-//   return (
-//     <Canvas camera={{ position: [10, 10, 10], fov: 60 }}>
-//       <OrbitControls enableZoom={true} enableRotate={orbitControlsEnabled} enablePan={orbitControlsEnabled} />
-//
-//       {/* Окружающий свет */}
-//       <ambientLight intensity={0.5} />
-//
-//       {/* Направленный свет */}
-//       <directionalLight
-//         position={[10, 10, 10]}
-//         intensity={1}
-//         castShadow={true}
-//       />
-//
-//       <ScaleProvider data={data}>
-//         {children}
-//         {/* Вызов ScaleHandler для передачи функций */}
-//         <ScaleHandler onScaleReady={onScaleReady} />
-//       </ScaleProvider>
-//     </Canvas>
-//   );
-// };
-//
-// // Вспомогательный компонент для передачи функций нормализации
-// const ScaleHandler: React.FC<{ onScaleReady: (scaleFunctions: ScaleFunctions) => void }> = ({ onScaleReady }) => {
-//   const scaleFunctions = useScale();  // scaleFUnctions updates constantly
-//
-//   useEffect(() => {
-//     onScaleReady(scaleFunctions); // Передаём функции вверх через callback
-//   }, [onScaleReady, scaleFunctions]);
-//
-//   return null;
-// };
-//
-// export default Scene;
