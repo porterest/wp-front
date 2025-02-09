aeedc6fdef5de1a9de2eb2b4c6bcf9d788bc5215
porterest <191361776+porterest@users.noreply.github.com>
2025-02-09 17:09:37 +0300

    candles

--- backend/app/routes/candle.py
+++ backend/app/routes/candle.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 
 from dependencies.services.candle import get_candle_service
 from domain.metaholder.responses.candle import Candle
+from domain.models.block import Block
 
 router = APIRouter(
     prefix="/candles",
@@ -20,23 +21,28 @@ async def get_candles(pair_id: str, n: int) -> Optional[list[Candle]]:
     logger.info("blocks!!!")
     logger.info(blocks)
     candles = []
+    prev_block: Optional[Block] = None
     for block in blocks:
         volume = sum([bet.amount for bet in block.bets])
         bet_prices = [bet.vector[0] for bet in block.bets]
         if not bet_prices and volume == 0:
-            return
-        low_price = min(bet_prices)
-        high_price = max(bet_prices)
+            low_price = 0
+            high_price = 0
+        else:
+            low_price = min(bet_prices)
+            high_price = max(bet_prices)
 
         candle = Candle(
-            opening_price=block.bets[0].vector[0],
-            closing_price=block.bets[-1].vector[0],
+            opening_price=prev_block.result_vector[0] if prev_block else 0,
+            closing_price=block.result_vector[0],
             high_price=high_price,
             low_price=low_price,
             volume=volume,
             block_number=block.block_number,
         )
         candles.append(candle)
+        prev_block = block
+
     logger.info("candles")
     logger.info(candles)
     return candles
