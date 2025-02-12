64dbf89601261dcb6930d4c45d2a935218af7481
porterest <191361776+porterest@users.noreply.github.com>
2025-02-12 11:18:55 +0300

    meow (frontend)

--- frontend/src/components/HistoricalVectors.tsx
+++ frontend/src/components/HistoricalVectors.tsx
@@ -115,11 +115,12 @@ const HistoricalVectors: React.FC<HistoricalVectorsProps> = ({
     for (let i = 0; i < count; i++) {
       console.log(`Входной вектор ${i}: [${vectors[i][0]}, ${vectors[i][1]}]`);
       // Вычисляем смещение: компоненты берутся из vectors, по оси Z задаем delta
-      console.log(`точка в которую надо придти [${currentPoint.x}, ${currentPoint.y}, delta]`);
-      const offset = new THREE.Vector3(vectors[i][0] - currentPoint.x, vectors[i][1]-currentPoint.y, delta);
-      console.log("Вектор смещения (offset):", offset.toArray());
+      // console.log(`точка в которую надо придти [${currentPoint.x}, ${currentPoint.y}, delta]`);
+      // const offset = new THREE.Vector3(vectors[i][0] - currentPoint.x, vectors[i][1]-currentPoint.y, delta);
+      // console.log("Вектор смещения (offset):", offset.toArray());
       // Вычисляем "конец вектора" как сумму текущей точки и offset
-      const nextPoint = currentPoint.clone().add(offset);
+      // const nextPoint = currentPoint.clone().add(offset);
+      const nextPoint = new THREE.Vector3(vectors[i][0], vectors[i][1], currentPoint.z+delta);
       console.log(
         `Вектор ${i}: начало вектора: ${currentPoint.toArray()}, конец вектора: ${nextPoint.toArray()}`
       );
