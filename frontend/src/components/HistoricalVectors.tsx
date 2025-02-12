90c9daf2f050e2ef83b8a91e5a0b1bc94df12290
porterest <191361776+porterest@users.noreply.github.com>
2025-02-12 16:06:30 +0300

    meow (frontend)

--- frontend/src/components/HistoricalVectors.tsx
+++ frontend/src/components/HistoricalVectors.tsx
@@ -124,7 +124,7 @@ const HistoricalVectors: React.FC<HistoricalVectorsProps> = ({
 // Создаем горизонтальный вектор из x и y
       const horizontal = new THREE.Vector2(vectors[i][1], vectors[i][0]);
 // Устанавливаем его длину равной 2
-      horizontal.setLength(2);
+      horizontal.setLength(delta);
 // Собираем итоговый offset, где z остаётся без изменений
       const newOffset = new THREE.Vector3(horizontal.x, horizontal.y, currentPoint.z + delta);
 // Вычисляем nextPoint как текущая точка плюс newOffset
