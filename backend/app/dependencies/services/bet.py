8fbde7f9c04edfb714daeb0241426d4c08253fac
porterest <191361776+porterest@users.noreply.github.com>
2025-02-14 17:28:48 +0300

    meow (backend)

--- backend/app/dependencies/services/bet.py
+++ backend/app/dependencies/services/bet.py
@@ -2,7 +2,6 @@ from abstractions.services.bet import BetServiceInterface
 from dependencies.repositories.bet import get_bet_repository
 from dependencies.repositories.block import get_block_repository
 from dependencies.repositories.user import get_user_repository
-from dependencies.services.chain import get_chain_service
 from services.BetService import BetService
 
 
@@ -10,6 +9,5 @@ def get_bet_service() -> BetServiceInterface:
     return BetService(
         bet_repository=get_bet_repository(),
         user_repository=get_user_repository(),
-        chain_service=get_chain_service(),
         block_repository=get_block_repository(),
     )
